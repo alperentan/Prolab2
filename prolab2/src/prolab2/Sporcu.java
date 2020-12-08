99a0d5fe04809c53c8df09333b799ca467baa21d
alperentan <56079264+alperentan@users.noreply.github.com>
2020-12-08 12:18:23 +0300

    Add files via upload

---
+++ prolab2/src/prolab2/Sporcu.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prolab2;
+
+/**
+ *
+ * @author alperentan
+ */
+public class Sporcu {//parent class
+
+    String sporcuIsim = "";
+    String sporcuTakim = "";
+
+    public Sporcu() {
+
+    }
+
+    Sporcu(String sporcuIsim, String sporcuTakim) {
+        this.sporcuIsim = sporcuIsim;
+        this.sporcuTakim = sporcuTakim;
+    }
+
+    public String getSporcuIsim() {
+        return sporcuIsim;
+    }
+
+    public void setSporcuIsim(String sporcuIsim) {
+        this.sporcuIsim = sporcuIsim;
+    }
+
+    public String getSporcuTakim() {
+        return sporcuTakim;
+    }
+
+    public void setSporcuTakim(String sporcuTakim) {
+        this.sporcuTakim = sporcuTakim;
+    }
+}
